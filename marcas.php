817bee39300ba396350ad6d5b1e72ad54dea23c2
Gabriel1113 <66348489+Gabriel1113@users.noreply.github.com>
2020-06-15 19:11:25 -0500

    Add files via upload

---
+++ marcas.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MARCAS TECNOLOGICAS</title>
+	<h1 align="center">Marcas Tecnologicas</h1>
+</head>
+<body>
+	<div align="center">
+	<table width="200" border="1">
+<tr>
+    <th>Marcas</th>
+    <th>Logo</th>
+    <th class="">
+    <?php
+    echo "Nombre color";
+    ?>
+    </th>
+    <th class="">
+    <?php
+    echo "Color";
+    ?>
+    </th>
+</tr>
+<tr>
+    <td>EPSON</td>
+    <td>
+        <a href="https://epson.com.ec/"> <img src="https://1000marcas.net/wp-content/uploads/2020/02/Epson-Logo.png" width="68" heigh="70"></a>
+        </td>
+        <td class="">
+        <?php
+        echo "MORADO";
+        echo "<td bgcolor = #5F05FE>";
+        ?>
+    </td>
+</tr>
+<tr>
+    <td>TOSHIBA</td>
+    <td>
+        <a href="https://www.toshiba.com/tai/"><img src="https://w7.pngwing.com/pngs/922/949/png-transparent-toshiba-hewlett-packard-laptop-logo-dell-hewlett-packard-text-trademark-logo.png" width="68" height="70"></a>
+        </td>
+        <td class="">
+        <?php
+        echo "ROJO";
+        echo "<td bgcolor = #FE1C05>";
+        ?>
+    </td>
+</tr>
+<tr>
+    <td>SONY</td>
+    <td>
+        <a href="https://www.sony.com.ec/"><img src="https://1000marcas.net/wp-content/uploads/2020/01/Sony.jpg" width="68" height="70"></a>
+    </td>
+    <td class="">
+        <?php
+        echo "NEGRO";
+        echo "<td bgcolor = #060606>";
+        ?>
+    </td>
+</tr>
+<tr>
+    <td>PHILIPS</td>
+    <td>
+        <a href="https://www.usa.philips.com/"><img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0000/2750/brand.gif?itok=g7ly-J5s" width="68" height="70"></a>  
+        </td>
+        <td class="">
+        <?php
+        echo "CELESTE";
+        echo "<td bgcolor = #22B3FB>";
+        ?>
+    </td>
+</tr>
+<tr>
+    <td>HISENSE</td>
+    <td>
+        <a href="https://www.hisense.es/"><img src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2019/02/hisense-logo.png?itok=8KEHXfTl" width="68" height="70"></a>
+        </td>
+        <td class="">
+        <?php
+        echo "TURQUESA";
+        echo "<td bgcolor = #28D9BC>";
+        ?>
+    </td>
+</tr>
+</table>
+</div>
+</body>
+</html>
